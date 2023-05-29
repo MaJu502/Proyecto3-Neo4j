2b4d29c9a46f32ec0d16b004cb99a868e186f738
MarcoJurado20308 <60157453+MaJu502@users.noreply.github.com>
2023-05-28 19:35:33 -0600

    .

---
+++ Backend/backmain.py
@@ -0,0 +1,4 @@
+from neo4j import GraphDatabase
+import logging
+from datetime import datetime
+from neo4j.exceptions import ServiceUnavailable
---
+++ main.py
@@ -0,0 +1,7 @@
+'''Universidad del valle de guatemala
+Marco Jurado, Cristian Aguirre, Rodirgo Barrera'''
+
+if __name__ == "__main__":
+    uri = "neo4j+s://b5ae0841.databases.neo4j.io "
+    user = "neo4j"
+    password = "L7lYs5A1-8jg-tDnY6Fs52C3jAfz42CTHaeJvfGSgOc"
