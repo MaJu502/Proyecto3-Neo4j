07fa1c6364db476fa70eb9d2d3b7c78dc99a06de
MarcoJurado20308 <60157453+MaJu502@users.noreply.github.com>
2023-05-28 19:57:10 -0600

    init y close de backed

--- Backend/backmain.py
+++ Backend/backmain.py
@@ -1,4 +1,14 @@
+'''Universidad del valle de guatemala
+Marco Jurado, Cristian Aguirre, Rodrigo Barrera
+app.py'''
+
 from neo4j import GraphDatabase
 import logging
 from datetime import datetime
 from neo4j.exceptions import ServiceUnavailable
+
+class App:
+    def __init__(self, uri, user, password) -> None:
+        self.driver = GraphDatabase.driver(uri, auth=(user, password))
+    def close(self):
+        self.driver.close()
--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 '''Universidad del valle de guatemala
 Marco Jurado, Cristian Aguirre, Rodirgo Barrera'''
+from Backend.backmain import App
+
 
 if __name__ == "__main__":
     uri = "neo4j+s://b5ae0841.databases.neo4j.io "
     user = "neo4j"
     password = "L7lYs5A1-8jg-tDnY6Fs52C3jAfz42CTHaeJvfGSgOc"
+
+    app = App(uri,user,password) #instanciar backend y la conexion a Cypher.
+
+
+
+    #if salir del frontend
+    app.close()
